856d231a148f602d0eec1cd1084cab5a68f80f7a
MacMichas <MacMichas@MacMichass-MacBook-Pro.local>
2013-10-20 04:56:15 -0200

    Initial Commit

---
+++ Radio Novo Tempo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Radio Novo Tempo
+//
+//  Created by MacMichas on 10/20/13.
+//  Copyright (c) 2013 Novo Tempo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Radio Novo Tempo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Radio Novo Tempo
+//
+//  Created by MacMichas on 10/20/13.
+//  Copyright (c) 2013 Novo Tempo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
